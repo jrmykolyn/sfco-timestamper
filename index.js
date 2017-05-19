3e510caa65e8adbd3c698cbbc6a4aeee2e47edf0
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-19 17:20:25 -0400

    Added pseudocode for program functionality and initialization logic in 'index.js'.

--- index.js
+++ index.js
@@ -18,3 +18,20 @@
 // --------------------------------------------------
 // INIT
 // --------------------------------------------------
+// CHECK FOR `GLOBAL_CONFIG` IN USER HOME DIR.
+// IF NOT FOUND
+    // PROMPT INIT
+        // IF INIT ACCEPTED
+            // DO INIT
+            // CREATE `GLOBAL_CONFIG`
+            // PRINT MESSAGE: 'Initialization complete'
+        // ELSE
+            // PRINT MESSAGE: 'Initialization required'
+            // EXIT
+// READ `GLOBAL_CONFIG`
+// VALIDATE PRESENCE OF `path` KEY (EG. 'target' REPO)
+// VALIDATE PRESENCE OF `README.md` FILE IN 'target' REPO
+// ADD TIMESTAMP TO `README.md` FILE
+// STAGE AND COMMIT UPDATES
+// PRINT MESSAGE: 'Updated "README" and committed changes'
+// EXIT
