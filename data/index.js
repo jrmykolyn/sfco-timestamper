eb7975d7ac1da192b2a7ace01584cee371e08c24
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-24 08:42:09 -0400

    Added new 'limit' string to 'MESSAGES'.

--- data/index.js
+++ data/index.js
@@ -6,7 +6,8 @@ const MESSAGES = {
 		success: 'Yay! You\'ve successfully padded your repo contributions!\n'
 	},
 	error: {
-		default: 'Whoops! Something went wrong'
+		default: 'Whoops! Something went wrong',
+		limit: 'Whoops! It looks like `timestamper` has already been invoked today. If you absolutely must pad your repo activity with another meaningless commit, add the `--force` flag.'
 	}
 };
 
