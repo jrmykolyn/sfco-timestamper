9d8e58ca5b00096d92919d363e5af9a295ff6205
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-24 08:43:22 -0400

    Moved bulk of initialization logic from 'index.js' to 'timestamper.js'. Refactored/renamed methods.

--- index.js
+++ index.js
@@ -3,30 +3,13 @@
 // --------------------------------------------------
 // IMPORT MODULES
 // --------------------------------------------------
-// Node
-const os = require( 'os' );
-const fs = require( 'fs' );
-const path = require( 'path' );
-const exec = require( 'child_process' ).exec;
-const StringDecoder = require( 'string_decoder' ).StringDecoder;
-
-// Vendor
-var literati = require( 'sfco-literati' );
-
 // Project
 const timestamper = require( './lib/timestamper' );
-const CONFIG = require( './config' )
 
 // --------------------------------------------------
 // DECLARE VARS
 // --------------------------------------------------
-const decoder = new StringDecoder( 'utf8' );
 const ARGS = process.argv.slice( 2 );
-
-var GLOBAL_CONFIG = CONFIG.configFile;
-var REPO_PATH = CONFIG.repoPath;
-var REPO_NAME = CONFIG.repoName;
-var DUMP_FILE = CONFIG.dumpFile;
 
 // --------------------------------------------------
 // INIT
@@ -40,62 +23,10 @@ var DUMP_FILE = CONFIG.dumpFile;
 // - Append `DUMP_FILE` with message/timestamp for current day.
 // - Commit updates.
 // - Log 'success' message and git output.
-timestamper.init( `${os.homedir()}/${GLOBAL_CONFIG}` )
-	.then( ( data ) => { return decoder.write( data ); } )
-	.then( ( data ) => { return JSON.parse( data ); } )
-	.then( ( data ) => {
-		// Update internal vars. with values extracted from config.
-		REPO_PATH = ( data.repoPath && typeof data.repoPath === 'string' ) ? data.repoPath : REPO_PATH;
-		REPO_NAME = ( data.repoName && typeof data.repoName === 'string' ) ? data.repoName : REPO_NAME;
-		DUMP_FILE = ( data.dumpFile && typeof data.dumpFile === 'string' ) ? data.dumpFile : DUMP_FILE;
-
-		// ...
-		return `${REPO_PATH}/${REPO_NAME}/${DUMP_FILE}`;
-	} )
-	.then( ( pathStr ) => { return timestamper.resolvePath( pathStr ); } )
-	.then( ( pathStr ) => {
-		return literati.read( pathStr )
-			.then(
-				( data ) => {
-					if ( !decoder.write( data ).includes( timestamper.getTimestamp() ) || ARGS.includes( '--force' ) ) {
-						return pathStr;
-					} else {
-						throw new Error( 'Whoops! It looks like `timestamper` has already been invoked today. If you absolutely must pad your repo activity with another meaningless commit, add the `--force` flag.' );
-					}
-				},
-				( err ) => {
-					throw new Error( `Whoops! ${pathStr} directory either doesn't exist, or doesn't contain ${DUMP_FILE}. Please ensure that ${path.dirname( pathStr )} exists, is a directory, and contains the ${DUMP_FILE} file before invoking the \`timestamper\` command.` );
-				}
-			)
- 	} )
-	.then( ( pathStr ) => {
-		return new Promise( ( resolve, reject ) => {
-			fs.appendFile( pathStr, timestamper.getDumpMsg( ARGS.includes( '--force' ) ), 'utf8', ( err, data ) => {
-				if ( err ) {
-					reject( err );
-				} else {
-					resolve( pathStr );
-				}
-			} )
-		} );
-	} )
-	.then( ( pathStr ) => {
-		return new Promise( ( resolve, reject ) => {
-			process.chdir( path.dirname( pathStr ) );
-
-			var commitMsg = timestamper.getCommitMsg( ARGS.includes( '--force' ) );
-
-			exec( `git add ${DUMP_FILE} && git commit -m "${commitMsg}"`, ( err, stdout, stderr ) => {
-				if ( err ) {
-					throw new Error( `Whoops. Failed to commit updated ${DUMP_FILE} file.` );
-				}
-
-				resolve( stdout );
-			} );
-		} )
-	} )
+/// TODO[@jrmykolyn] - Move comments into `lib/timestamper.js`.
+timestamper.init( ARGS )
 	.then( ( output ) => {
-		timestamper.log( 'process', 'success' );
+		console.log( timestamper.getMsg( 'process', 'success' ) );
 		console.log( output );
 	} )
 	.catch(
@@ -103,7 +34,7 @@ timestamper.init( `${os.homedir()}/${GLOBAL_CONFIG}` )
 			if ( err.message ) {
 				console.log( err.message );
 			} else {
-				timestamper.log( 'error', 'default' );
+				console.log( timestamper.getMsg( 'error', 'default' ) );
 			}
 		}
 	);
