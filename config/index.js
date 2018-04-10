2ff018b058b0a24203fb259eebadab99114be0a2
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-09 21:31:05 -0400

    topic-1 Consolidate contents of 'data/' with 'config/'. Export 'getMsg()'.

--- config/index.js
+++ config/index.js
@@ -3,4 +3,13 @@ module.exports = {
 		repoPath: '~/timestamper-dump',
 		dumpFile: 'DUMP.md'
 	},
+	messages: {
+		process: {
+			success: 'Yay! You\'ve successfully padded your repo contributions!'
+		},
+		error: {
+			default: 'Whoops! Something went wrong',
+			limit: 'Whoops! It looks like `timestamper` has already been invoked today. If you absolutely must pad your repo activity with another meaningless commit, add the `--force` flag.'
+		}
+	},
 };
